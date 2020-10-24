b551661130096cac700b27f2132e52fc32bc6ba1
isaaclim95 <isaaclim95@gmail.com>
2020-10-24 20:06:53 +1000

    trying to merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:isaacs_app/ui/views/startup/startup_view.dart';
@@ -13,7 +14,35 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget  {
+  @override
+  _MyAppState createState() => _MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+
+  final CollectionReference userCollection =
+  FirebaseFirestore.instance.collection("users");
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+
+    if (state == AppLifecycleState.resumed) {
+      // User is online
+      userCollection.doc("isOnline").update({"isOnline":"true"});
+    } else if (state == AppLifecycleState.inactive) {
+      // User is offline
+      userCollection.doc("isOnline").update({"isOnline":"false"});
+    }
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/ui/models/user_model.dart
+++ lib/ui/models/user_model.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
-
-class UserModel extends ChangeNotifier {
+class UserModel {
   String name;
   int age;
   double weight;
